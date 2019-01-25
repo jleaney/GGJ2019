18f7377e502f862c71b6b0037429c03110eb3c9f
Howl-Pendragon <36288792+Howl-Pendragon@users.noreply.github.com>
2019-01-26 08:54:09 +1100

    Overlay instance & beginning of world grid.

---
+++ Assets/Scripts/Overlay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overlay : MonoBehaviour
+{
+	public static Overlay instance;
+
+	private void Awake()
+	{
+		if (instance == null) instance = this;
+		else throw new Exception("Duplicate instance of Overlay singleton.");
+	}
+
+	public void FadeOut(float duration)
+	{
+		_overlay.DOFade(0.0f, duration);
+	}
+
+	public void FadeIn(float duration)
+	{
+		FadeIn(duration, Color.white);
+	}
+
+	public void FadeIn(float duration, Color color)
+	{
+		_overlay.color = color;
+		_overlay.DOFade(1.0f, duration);
+	}
+
+	[SerializeField] private Image _overlay;
+}
---
+++ Assets/Scripts/WorldGrid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGrid : MonoBehaviour
+{
+	public static WorldGrid instance;
+
+	private void Awake()
+	{
+		if (instance == null) instance = this;
+		else throw new Exception("Duplicate instance of WorldGrid singleton.");
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		for (int i = -9; i < 9; i++)
+		{
+			for (int j = -9; j < 9; j++)
+			{
+				Gizmos.DrawWireCube(
+					new Vector3(i * gridWidth, 0, j * gridWidth),
+					new Vector3(gridWidth, 0, gridWidth));
+			}
+		}
+	}
+
+	public static Vector3 SnapToGrid(Vector3 position)
+	{
+
+	}
+
+	[SerializeField] private float gridWidth = 1;
+}
