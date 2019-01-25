e94ca997eb34473a22b7c7bfab4afc2daed22e9c
Jacob Leaney <jacobleaney@gmail.com>
2019-01-26 09:58:07 +1100

    Draw 1 animation works

---
+++ Assets/Scripts/DrawerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerController : MonoBehaviour {
+
+    private bool open = false;
+
+    private void Start()
+    {
+    }
+
+    private void OnMouseDown()
+    {
+        if (!open)
+        {
+            open = true;
+            GetComponent<Animator>().SetTrigger("open");
+        }
+
+        else if (open)
+        {
+            open = false;
+            GetComponent<Animator>().SetTrigger("close");
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        
+        
+    }
+
+}
