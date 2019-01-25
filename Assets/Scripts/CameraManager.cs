f57b0322b4459d4dc72c7293fae6cc16d335bd43
Howl-Pendragon <36288792+Howl-Pendragon@users.noreply.github.com>
2019-01-26 01:49:24 +1100

    Set up preliminary sprite materials, post processing & rendering

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+	public static CameraManager instance;
+	public Camera camera;
+
+	private void Awake()
+	{
+		if (instance == null) instance = this;
+		else throw new Exception("Duplicate instance of CameraManager singleton.");
+		camera = GetComponentInChildren<Camera>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+			transform.Rotate(transform.up, -90);
+
+		if (Input.GetKeyDown(KeyCode.E))
+			transform.Rotate(transform.up, 90);
+	}
+}
