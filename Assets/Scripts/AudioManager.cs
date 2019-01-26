22c961b73c42d660c77508d3512ad301086f5d70
Jacob Leaney <jacobleaney@gmail.com>
2019-01-26 12:37:37 +1100

    Added ambient track and musoc track

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip[] ambientSounds, musicTracks;
+    private AudioClip defaultAmbience;
+
+    private Dictionary<string, AudioClip> ambSoundsDict = new Dictionary<string, AudioClip>();
+
+    private AudioSource music, ambience;
+
+	void Start () {
+
+        music = transform.GetChild(0).GetComponent<AudioSource>();
+        ambience = transform.GetChild(1).GetComponent<AudioSource>();
+
+        ambience.clip = ambientSounds[0];
+        ambience.Play();
+
+        music.clip = musicTracks[Random.Range(0, musicTracks.Length - 1)];
+        music.Play();
+	}
+	
+	void Update () {
+		
+	}
+}
