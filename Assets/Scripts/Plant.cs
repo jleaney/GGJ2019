130fba3b28e9c471f75303e67bd3cddc8433599a
Jacob Leaney <jacobleaney@gmail.com>
2019-01-26 15:06:02 +1100

    Updated Drawer, began prefabbing plants

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class Plant : Pickup
 {
+    [SerializeField]
+    private Material plant;
 
+    [SerializeField]
+    private float scaleMin, scaleMax;
+
+    [SerializeField]
+    private GameObject[] grassCombos;
+    private void Start()
+    {
+        // set seeding
+    }
+
+    private void SetRandomSize()
+    {
+        float size = Random.Range(scaleMin, scaleMax);
+        transform.localScale = new Vector3(size, size, size);
+    }
+
+    private void SetGrassCombo()
+    {
+        Instantiate(grassCombos[Mathf.FloorToInt(Random.Range(0, grassCombos.Length))], transform.parent);
+    }
+
+    private void Grow()
+    {
+        // seedling grow animation
+    }
+
+    public void CompleteGrowing()
+    {
+        transform.GetComponentInChildren<Renderer>().material = plant;
+
+        SetRandomSize();
+
+        SetGrassCombo();
+    }
 }
